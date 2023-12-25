87f0dfba96407008abcceb0762fdcff9a5d72f38
LeonhartAnnie <122689484+LeonhartAnnie@users.noreply.github.com>
2023-12-25 16:49:09 +0800

    角色移動
    就醬

--- Assets/ForCamera_CS/CubeClickDetection.cs
+++ Assets/ForCamera_CS/CubeClickDetection.cs
@@ -22,8 +22,9 @@ public class CubeClickDetection : MonoBehaviour
             {
                 if (hit.collider.CompareTag("Ground_Cube")) // 检测是否点击到了 Cube
                 {
-                    Vector3 cubePosition = hit.collider.transform.position;
-                    Debug.Log("Clicked on "+hit.collider.name+" at position: " + cubePosition);
+                    CubeManager.Cube_position = hit.collider.transform.position;
+                    CubeManager.Cube_position = new Vector3(CubeManager.Cube_position.x, CubeManager.Cube_position.y + 6, CubeManager.Cube_position.z);
+                    Debug.Log("Clicked on "+hit.collider.name+" at position: " + CubeManager.Cube_position);
                 }
             }
         }
---
+++ Assets/For_Class/CubeManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CubeManager 
+{
+    public static Vector3 Charactor_spawn_position;
+    public static Vector3 Cube_position;
+    
+}
